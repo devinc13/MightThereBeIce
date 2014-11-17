5f87b020f08548bd827b33bd5d7d575b9a9ed3d2
devin <devincorrigall@gmail.com>
2014-11-16 18:24:03 -0800

    Added functionality to loop through past 24 hours of data, and get an idea of if there might be ice.

--- app/src/main/java/com/c13/devin/mighttherebeice/LocationSelectFragment.java
+++ app/src/main/java/com/c13/devin/mighttherebeice/LocationSelectFragment.java
@@ -2,6 +2,7 @@ package com.c13.devin.mighttherebeice;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.List;
 
 import DataObjects.ApiData;
@@ -46,11 +48,25 @@ public class LocationSelectFragment extends Fragment {
                 } else {
                     RestAdapter restAdapter = new RestAdapter.Builder()
                             .setEndpoint("http://api.worldweatheronline.com")
+                            .setLogLevel(RestAdapter.LogLevel.FULL)
+                            .setLog(new RestAdapter.Log() {
+                                @Override
+                                public void log(String msg) {
+                                    Log.i("ApiRequest", msg);
+                                }
+                            })
                             .build();
 
                     RequestManager service = restAdapter.create(RequestManager.class);
 
-                    subscription = service.getData(location, Keys.getWeatherApiKey())
+                    // Start at yesterday and end at today
+                    Calendar c = Calendar.getInstance();
+                    // Month is 0 indexed
+                    String endDate = c.get(Calendar.YEAR) + "-" + (c.get(Calendar.MONTH) + 1) + "-" + c.get(Calendar.DATE);
+                    c.add(Calendar.DATE, -1);
+                    String startDate = c.get(Calendar.YEAR) + "-" + (c.get(Calendar.MONTH) + 1) + "-" + c.get(Calendar.DATE);
+
+                    subscription = service.getData(location, Keys.getWeatherApiKey(), startDate, endDate)
                             .subscribeOn(Schedulers.io())
                             .observeOn(AndroidSchedulers.mainThread())
                             .subscribe(new Observer<ApiData>() {
@@ -71,12 +87,51 @@ public class LocationSelectFragment extends Fragment {
 
                                 @Override
                                 public void onNext(ApiData apiData) {
+                                    Boolean below0 = false;
+                                    Boolean precipitation = false;
                                     String weather = "Location = " + apiData.getData().getRequest().get(0).getQuery() + "\n";
-                                    weather = weather + "Date = " + apiData.getData().getWeather().get(0).getDate() + "\n";
-                                    List<Hourly> hourly = apiData.getData().getWeather().get(0).getHourly();
-                                    for (int i = 0; i < hourly.size(); i++) {
-                                        weather = weather + hourly.get(i).getTime() + " : " + hourly.get(i).getTempC() + " °C\n";
+                                    Calendar c = Calendar.getInstance();
+                                    int hour = c.get(Calendar.HOUR_OF_DAY) * 100;
+                                    int count = 0;
+
+                                    for (int i = 1; i >= 0; i--) {
+                                        //weather = weather + "Date = " + apiData.getData().getWeather().get(i).getDate() + "\n";
+                                        List<Hourly> hourly = apiData.getData().getWeather().get(i).getHourly();
+                                        for (int j = hourly.size() - 1; j >= 0; j--) {
+                                            // Check if the time is before the current time, or if we have passed into the previous day
+                                            if (Integer.valueOf(hourly.get(j).getTime())> hour || i == 0) {
+                                                if (count > 8) {
+                                                    break;
+                                                }
+
+                                                count++;
+
+                                                if (Integer.valueOf(hourly.get(j).getTempC()) < 0.0) {
+                                                    below0 = true;
+                                                }
+
+                                                if (Float.valueOf(hourly.get(j).getPrecipMM()) > 1.0) {
+                                                    precipitation = true;
+                                                }
+                                                weather = weather + "Date = " + apiData.getData().getWeather().get(i).getDate() + " Time = " + hourly.get(j).getTime() + " Temp = " + hourly.get(j).getTempC() + "°C Precipitation = " + hourly.get(j).getPrecipMM() + " mm\n";
+                                            }
+                                        }
                                     }
+
+                                    String severity;
+                                    String reason;
+                                    if (below0 && precipitation) {
+                                        severity = "High";
+                                        reason = "There was precipitation and the temperature was below 0 degrees Celsius in the last 24 hours.";
+                                    } else if (below0) {
+                                        severity = "Medium";
+                                        reason = "The temperature was below 0 degrees Celsius in the last 24 hours.";
+                                    } else {
+                                        severity = "Low";
+                                        reason = "The temperature hasn't been below 0 degrees Celsius in the last 24 hours.";
+                                    }
+
+                                    weather = weather + "\n \nThere is a " + severity + " chance of ice.\n" + reason;
 
                                     textView.setText(weather);
                                 }
--- app/src/main/java/com/c13/devin/mighttherebeice/RequestManager.java
+++ app/src/main/java/com/c13/devin/mighttherebeice/RequestManager.java
@@ -6,6 +6,6 @@ import retrofit.http.Query;
 import rx.Observable;
 
 public interface RequestManager {
-    @GET("/free/v2/past-weather.ashx?format=json&extra=localObsTime%2CisDayTime&date=today")
-    Observable<ApiData> getData(@Query("q") String location, @Query("key") String key);
+    @GET("/free/v2/past-weather.ashx?format=json&extra=localObsTime%2CisDayTime")
+    Observable<ApiData> getData(@Query("q") String location, @Query("key") String key, @Query("date") String date, @Query("enddate") String enddate);
 }
